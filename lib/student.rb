cadf2f92ccc2fd4ce9a88dc897b01e7d64e3fca6
Jon Mackey <jon@jonmackey.party>
2019-01-28 21:49:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,17 @@ class Student
     
     DB[:conn].execute(sql)
   end
+  
+  def self.first_X_students_in_grade_10(num) 
+    sql = <<-SQL 
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      LIMIT ? 
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 
 
   def self.find_by_name(name)
